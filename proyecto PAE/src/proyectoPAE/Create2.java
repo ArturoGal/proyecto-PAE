93f1ff0c6389dc19369bd35a6284fe52bd83507d
ArturoGal <arturogal95@gmail.com>
2017-10-26 21:02:42 -0500

    Add files via upload

---
+++ proyecto PAE/src/proyectoPAE/Create2.java
@@ -0,0 +1,75 @@
+package proyectoPAE;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Create2 extends Application{
+
+		public void start(Stage primaryStage) {
+			//Instancia del Grid Pane
+			GridPane gp = new GridPane(); 
+			//Configura el padding
+			gp.setPadding(new Insets(10, 10, 10, 10));
+			//Se declara la etiqueta del nombre
+			Text create = new Text("Create");
+			Label lblTitle =  new Label("Title:"); 
+			Label lblDes= new Label("Description:"); 
+			Label lblSub = new Label("Subject:"); 
+			Label lblTool = new Label("Tool:"); 
+			TextField txtTitle = new TextField(); 
+			TextArea txtDes= new TextArea(); 
+			ChoiceBox<String> cbSub = new ChoiceBox<>(); 
+			CheckBox cb1 = new CheckBox("Mind Map");
+			CheckBox cb2 = new CheckBox("Flash Card");
+			CheckBox cb3 = new CheckBox("Chart");
+			CheckBox cb4 = new CheckBox("Table");
+			Button ok = new Button("OK"); 
+			
+			create.setStyle("-fx-font-size: 22;  ");
+			
+			//Espaciado entre columnas y filas 
+			gp.setVgap(10); 
+		    gp.setHgap(10);
+		    txtDes.setPrefColumnCount(20);
+		    txtDes.setPrefRowCount(4);
+		    txtTitle.setPrefColumnCount(20);
+		    
+		    //Se agregan los nodos al GridPane
+		    gp.add(create, 0,0); 
+		    gp.add(lblTitle, 0, 1);
+		    gp.add(lblDes, 0, 2);
+		    gp.add(lblSub, 0, 3);
+		    gp.add(lblTool, 0, 4);
+		    gp.add(ok, 5, 7); 
+		    gp.add(txtTitle, 2, 1);
+		    gp.add(txtDes, 2, 2);
+		    gp.add(cbSub, 2, 3);
+		    gp.add(cb1, 2, 4);
+		    gp.add(cb2, 2, 5);
+		    gp.add(cb3, 2, 6); 
+		    gp.add(cb4, 2, 7);
+		    
+		    //Se agrega el GridPane al Scence
+		    Scene scene = new Scene (gp); 
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//Se ajustan las prioridades del Stage y se agrega el Scence
+			primaryStage.setTitle("GridPane Example"); 
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		
+		public static void main(String[] args) {
+			launch(args);
+		}
+
+}
---
+++ proyecto PAE/src/proyectoPAE/Subjects2.java
@@ -0,0 +1,63 @@
+package proyectoPAE;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Subjects2 extends Application {
+	public void start(Stage stage) throws Exception {
+		GridPane grid = new GridPane();
+		grid.setPrefSize(475, 300);
+		grid.setHgap(15);
+		grid.setVgap(15);
+		grid.setPadding(new Insets(15));
+		Scene scene = new Scene(grid);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+		Label lb1 = new Label("SUBJECTS");
+		Label lb2 = new Label("Enter text");
+		ListView<String> lv = new ListView<String>();
+		TextField tf = new TextField();
+		Button bt1 = new Button("+ ADD");
+		Button bt2 = new Button("DELETE");
+		
+		lb1.setStyle("-fx-font-size: 24px");
+		
+
+		ObservableList<String> listItems = FXCollections.observableArrayList("Add Items here");
+		
+		lv.setItems(listItems);
+		GridPane.setHalignment(lb1, HPos.CENTER);
+		GridPane.setHalignment(bt1, HPos.CENTER);
+		GridPane.setHalignment(bt2, HPos.CENTER);
+		GridPane.setValignment(bt2, VPos.TOP);
+
+		grid.add(lb1, 0, 0, 2, 1);
+		grid.add(lv, 0, 1, 1, 4);
+		grid.add(lb2, 1, 1, 1, 1);
+		grid.add(tf, 1, 2, 1, 1);
+		grid.add(bt1, 1, 3, 1, 1);
+		grid.add(bt2, 1, 4, 1, 1);
+
+		
+	
+		stage.setScene(scene);
+		stage.show();
+		stage.setTitle("Study Buddy");
+		
+	}	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
